0139d2a98e1a5123de7138e568d1f28d75fc9fb9
Jared <jared.gallina@gmail.com>
2020-10-26 21:25:06 -0400

    Outline of first file

---
+++ MouseControl.py
@@ -0,0 +1,37 @@
+"""
+This module will be responsible for holding a class that
+allows for easy interfacing with Mouse Dropping Servos.
+
+    Class:    MouseControl
+    Functions:      - __init__
+                    - release_mouse
+                    - reset
+"""
+
+# importing necessary libraries
+import pigpio as ppo
+
+"""
+MouseControl - a class that will store an I/O connection and provide
+                an easy API for releasing mice into the Cat_Mouse_Launcher
+                device.
+                
+        Fields:
+        -------
+        
+        
+        Methods:
+        --------
+        
+"""
+
+class MouseControl:
+    
+    def __init__():
+        pass
+    
+    def release_mouse():
+        pass
+    
+    def reset():
+        pass
